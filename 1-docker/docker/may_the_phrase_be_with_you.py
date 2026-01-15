7f702bf9d42500965bb7eba7ac804dd985429358
Zac Oldroyd <137462136+zoldroyd@users.noreply.github.com>
2026-01-15 15:07:56 -0700

    Update may_the_phrase_be_with_you.py

--- 1-docker/docker/may_the_phrase_be_with_you.py
+++ 1-docker/docker/may_the_phrase_be_with_you.py
@@ -3,24 +3,24 @@ import pyfiglet
 
 
 PHRASES = (
-    "It's a trap!",
-    "Stay on target.",
-    "That's no moon.",
-    "I have spoken.",
-    "Let the wookiee win.",
-    "Dagobah.",
-    "Let go, Luke!",
-    "I know.",
-    "My kind of scum.",
-    "Sorry about the mess.",
-    "All of it.",
-    "You'll be dead!",
-    "Oh, the uniform.",
-    "TK421",
-    "Punch it!",
+    # "It's a trap!",
+    # "Stay on target.",
+    # "That's no moon.",
+    # "I have spoken.",
+    # "Let the wookiee win.",
+    # "Dagobah.",
+    # "Let go, Luke!",
+    # "I know.",
+    # "My kind of scum.",
+    # "Sorry about the mess.",
+    # "All of it.",
+    # "You'll be dead!",
+    # "Oh, the uniform.",
+    # "TK421",
+    # "Punch it!",
     "There is another",
     "I don't like sand.",
-    "He's holding me back!"
+    # "He's holding me back!"
 )
 
 def star_wars_quote():
